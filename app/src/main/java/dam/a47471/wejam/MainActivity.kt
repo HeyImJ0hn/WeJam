87ee64e45c2b2084ab598cf2fc76d9eb6b212a69
João <jpires.dev@gmail.com>
2023-05-04 11:19:27 +0100

    Improved Login UI

--- app/src/main/java/dam/a47471/wejam/MainActivity.kt
+++ app/src/main/java/dam/a47471/wejam/MainActivity.kt
@@ -3,21 +3,9 @@ package dam.a47471.wejam
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import dam.a47471.wejam.databinding.ActivityLoginBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        binding.signupBtn.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-        }
-
     }
 }
